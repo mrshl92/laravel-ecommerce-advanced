14bbf28a59a641bc73bef535c2f5ebd0e1171a6f
mouhssine elghazzali <mouhssine.elghazzali@gmail.com>
2020-06-13 23:09:58 +0200

    migration Tables MainCategories And languages

---
+++ database/migrations/2020_06_13_200030_create_languages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLanguagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('languages', function (Blueprint $table) {
+            $table->id();
+            $table->string('abbr',10);
+            $table->string('locale',20);
+            $table->string('name',100);
+            $table->enum('direction', ['rtl', 'ltr'])->default('rtl');
+            $table->tinyInteger('active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('languages');
+    }
+}
---
+++ database/migrations/2020_06_13_202936_create_main_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMainCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('main_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('translation_lang',10);
+            $table->unsignedInteger('translation_of');
+            $table->string('name',150);
+            $table->string('slug',150)->nullable();
+            $table->string('photo',150)->nullable();
+            $table->tinyInteger('active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('main_categories');
+    }
+}
